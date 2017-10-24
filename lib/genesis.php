d28332d7e3d86e0431777b21365cd8b7de04c10b
Michael Wender <michael@michaelwender.com>
2017-10-24 15:50:58 -0400

    Adding RJ Disclaimer

--- lib/genesis.php
+++ lib/genesis.php
@@ -28,3 +28,16 @@ function footer_creds_filter( $creds ) {
     return $creds;
 }
 add_filter( 'genesis_footer_creds_text', __NAMESPACE__ . '\\footer_creds_filter' );
+
+function footer_disclaimer(){
+?>
+<div class="disclaimer">
+<p>Raymond James financial advisors may only conduct business with residents of the states and/or jurisdictions for which they are properly registered. Therefore, a response to a request for information may be delayed. Please note that not all of the investments and services mentioned are available in every state. Investors outside of the United States are subject to securities and tax regulations within their applicable jurisdictions that are not addressed on this site. Contact your local Raymond James office for information and availability.</p>
+
+<p>Links are being provided for information purposes only. Raymond James is not affiliated with and does not endorse, authorize or sponsor any of the listed websites or their respective sponsors. Raymond James is not responsible for the content of any website or the collection or use of information regarding any website's users and/or members.</p>
+
+<p>Investment advisory services offered through Raymond James Financial Services Advisors, Inc. Marzolf Investment Group, LLC is not a registered broker/dealer and is independent of Raymond James Financial Services.</p>
+</div>
+<?php
+}
+add_action( 'genesis_footer', __NAMESPACE__ . '\\footer_disclaimer', 9 );
