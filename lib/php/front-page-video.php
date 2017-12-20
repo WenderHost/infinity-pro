0dc0ed82320c484d670806fe8f853f4e092e8a74
Michael Wender <michael@michaelwender.com>
2017-12-20 10:55:08 -0500

    Updating video embed plugin shortcode

--- lib/php/front-page-video.php
+++ lib/php/front-page-video.php
@@ -1,6 +1,6 @@
 <!-- Front page video -->
 <a name="front-page-video"></a>
 <div class="front-page-video">
-  <?= do_shortcode('[KGVID]https://marzolfinvestmentgroup.com/wp-content/uploads/2017/12/marzolf_intro.03.mov[/KGVID]') ?>
+  <?= do_shortcode('[evp_embed_video url="' . site_url( '/wp-content/uploads/2017/12/marzolf_intro.03.mov' ) . '" ratio="0.45" class="fp-playful" poster="' . site_url( '/wp-content/uploads/poster-frame.jpg' ) . '"]') ?>
 </div>
 <!-- /Front page video -->
