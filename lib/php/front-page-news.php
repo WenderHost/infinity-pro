dd967d1d852617277847ec57e5b5d6ce29f114c4
Michael Wender <michael@michaelwender.com>
2017-12-20 10:55:31 -0500

    Setup for News w/o posts

--- lib/php/front-page-news.php
+++ lib/php/front-page-news.php
@@ -1,6 +1,19 @@
 <!-- Front page news -->
+<?php
+$classes = explode( ' ', 'solid-section widget-area fadeup-effect fadeInUp' );
+$args = [
+    'posts_per_page' => 5,
+];
+$posts_array = get_posts( $args );
+if( $posts_array ){
+    $classes[] = 'flexible-widgets';
+    $classes[] = 'widget-halves';
+} else {
+    $classes[] = 'widget-centered';
+}
+?>
 <div id="front-page-news" class="front-page-news">
-    <div class="solid-section flexible-widgets widget-area fadeup-effect widget-halves fadeInUp">
+    <div class="<?= implode( ' ', $classes ) ?>">
         <div class="wrap">
             <section class="widget widget_text widget_custom_html">
                 <div class="widget_text widget-wrap">
@@ -9,24 +22,22 @@
             </section>
             <section class="widget widget_text">
                 <div class="widget_text widget-wrap">
-                <h3>Email Newsletter Signup</h3>
-                <p>Sign up below to receive all of our news posts via email:</p>
+                <h3 class="title">Email Newsletter Signup</h3>
+                <p class="caption">Sign up below to receive all of our news posts via email:</p>
                 <?php
-                //gravity_form(3,false,false);
                 $form_widget = new \MailPoet\Form\Widget();
                 echo $form_widget->widget(array('form' => 1, 'form_type' => 'php'));
                 ?>
                 </div>
             </section>
+            <?php
+            if( $posts_array ){
+            ?>
             <section class="widget widget_text">
                 <div class="widget_text widget-wrap">
                     <h3>Recent Posts</h3>
                     <ul class="post-list">
                     <?php
-                    $args = [
-                        'posts_per_page' => 5,
-                    ];
-                    $posts_array = get_posts( $args );
                     foreach( $posts_array as $post ){
                     ?>
                         <li><a href="<?= get_the_permalink( $post->ID ) ?>"><?= get_the_title( $post->ID ) ?></a></li>
@@ -37,6 +48,7 @@
                     </ul>
                 </div>
             </section>
+            <?php } ?>
         </div>
     </div>
 </div>
