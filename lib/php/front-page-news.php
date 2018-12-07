8917b8fb56c0921761c619f83e610d2df69fdd0c
Michael Wender <michael@michaelwender.com>
2018-12-07 12:50:00 -0500

    Updating front page news signup section

--- lib/php/front-page-news.php
+++ lib/php/front-page-news.php
@@ -15,15 +15,15 @@ if( $posts_array ){
 <div id="front-page-news" class="front-page-news">
     <div class="<?= implode( ' ', $classes ) ?>">
         <div class="wrap">
-            <section class="widget widget_text widget_custom_html">
+            <!--<section class="widget widget_text widget_custom_html">
                 <div class="widget_text widget-wrap">
                     <h3 class="widgettitle widget-title">News</h3>
                 </div>
-            </section>
+            </section>-->
             <section class="widget widget_text">
                 <div class="widget_text widget-wrap">
                 <h3 class="title">Email Newsletter Signup</h3>
-                <p class="caption">Sign up below to receive all of our news posts via email:</p>
+                <p class="caption">Sign up below to receive our email newsletter:</p>
                 <?php
                 $form_widget = new \MailPoet\Form\Widget();
                 echo $form_widget->widget(array('form' => 1, 'form_type' => 'php'));
